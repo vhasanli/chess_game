bc8ddba105c98b2c94f6ccd0b7ccdb7241dfdb78
vhasanli <vhasanov.software@gmail.com>
2025-03-29 20:43:18 -0500

    move_piece

--- chess.py
+++ chess.py
@@ -1,4 +1,4 @@
-board = [
+chess_board = [
     ["r", "n", "b", "q", "k", "b", "n", "r"],  # 8th rank (black pieces)
     ["p", "p", "p", "p", "p", "p", "p", "p"],  # 7th rank
     [".", ".", ".", ".", ".", ".", ".", "."],  # Empty squares
@@ -9,4 +9,27 @@ board = [
     ["R", "N", "B", "Q", "K", "B", "N", "R"]   # 1st rank
 ]
 
-print(board)
+board = [
+    ["b", ".", "."],
+    [".", ".", "."],
+    [".", ".", "."]
+]
+
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+
+def move_piece(board, old_row, old_col, new_row, new_col):
+    board[new_row][new_col] = board[old_row][old_col]
+    board[old_row][old_col] = "."  # clear old position
+
+# For example, move 'b' from (1, 2) to (0, 0)
+old_row = int(input("Enter oldrow: "))
+old_col = int(input("Enter old_col: "))
+new_row = int(input("Enter new_row: "))
+new_col = int(input("Enter new_col: "))
+
+move_piece(board, old_row, old_col, new_row, new_col)
+print_board(board)
+
+
