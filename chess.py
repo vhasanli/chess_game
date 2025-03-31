5ae495beef89705b94dd2e6da983118e5b885163
vhasanli <vhasanov.software@gmail.com>
2025-03-31 15:36:11 -0500

    Update chess.py, added movePawn func

--- chess.py
+++ chess.py
@@ -9,30 +9,23 @@ board = [
     ["R", "N", "B", "Q", "K", "B", "N", "R"]   # 1st rank
 ]
 
-board_2 = [
-    ["p", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", "r", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."]
-]
-
-def findRowCol(piece):
+def findRowCol(piece, col):
     found = False
     for row in range(len(board)):
         # print(f"row: {row}")
-        for col in range(len(board[0])):
-            # print(f"    col: {col}")
-            if board[row][col] == piece:
-                # print(board[row][col])
-                found = True    
-                break
-        if found:
-            print(f"Row and col of p: {row}, {col}")
-            return row, col
+        if board[row][col] == piece:
+            # print(board[row][col])
+            found = True    
+            break
+        # for col in range(len(board[0])):
+        #     # print(f"    col: {col}")
+        #     if board[row][col] == piece:
+        #         # print(board[row][col])
+        #         found = True    
+        #         break
+    if found:
+        print(f"Row and col of p: {row}, {col}")
+        return row, col
     
 def show_board(board):
     for row in board:
@@ -58,12 +51,12 @@ def pawnMoveRules(old_pos, new_pos):
         return 2
     elif (new_pos[0] == old_pos[0]) and (new_pos[1] == old_pos[1]):
         # Out of bounds
+        print("Out of bounds!")
         return 3
     else:
         print("Legal move")
-    # P can move only up one step at a time
-    return 0
-
+        return 0
+    
 def move_piece(board, piece):
     old_row, old_col = findRowCol(piece)
     
@@ -103,6 +96,25 @@ def move_piece(board, piece):
             board[new_row][new_col] = board[old_row][old_col]
             board[old_row][old_col] = "."  # clear old position
 
+def movePawn(board, piece, new_col):
+    old_row, old_col = findRowCol(piece, col)    
+    new_row = old_row
+    wrong_operator = True
+    while wrong_operator:
+        new_row+=1
+        new_row = checkBounds(new_row)
+        old_pos = (old_row, old_col)
+        new_pos = (new_row, new_col)
+        status = pawnMoveRules(old_pos, new_pos)
+        if status == 0:
+            board[new_row][new_col] = board[old_row][old_col]
+            board[old_row][old_col] = "."  # clear old position
+            break
+        else:
+            print("Wrong operator! Please use u, d, l, or r")
+            wrong_operator = True
+
+
 while True:
     # For example, move 'b' from (1, 2) to (0, 0)
     print("############################")
@@ -110,7 +122,9 @@ while True:
     print("############################")
 
     show_board(board)
-    piece = input("Which piece do you want to move?")
-    move_piece(board, piece)
+    piece = input("Which piece do you want to move? ")
+    col = int(input("Which column? "))
+    # move_piece(board, piece, col)
+    movePawn(board, piece, col)
 
 
