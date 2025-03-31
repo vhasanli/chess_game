f04384b35a869a50e6ceed7c3b3e395a4ec05960
vhasanli <vhasanov.software@gmail.com>
2025-03-30 20:43:21 -0500

    Which piece do you want to move?

--- chess.py
+++ chess.py
@@ -1,4 +1,4 @@
-chess_board = [
+board = [
     ["r", "n", "b", "q", "k", "b", "n", "r"],  # 8th rank (black pieces)
     ["p", "p", "p", "p", "p", "p", "p", "p"],  # 7th rank
     [".", ".", ".", ".", ".", ".", ".", "."],  # Empty squares
@@ -9,30 +9,29 @@ chess_board = [
     ["R", "N", "B", "Q", "K", "B", "N", "R"]   # 1st rank
 ]
 
-board = [
+board_2 = [
     ["p", ".", ".", ".", ".", ".", ".", "."],
     [".", ".", ".", ".", ".", ".", ".", "."],
     [".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", "r", ".", ".", ".", "."],
     [".", ".", ".", ".", ".", ".", ".", "."],
     [".", ".", ".", ".", ".", ".", ".", "."],
     [".", ".", ".", ".", ".", ".", ".", "."],
     [".", ".", ".", ".", ".", ".", ".", "."]
 ]
 
-def findRowCol():
+def findRowCol(piece):
     found = False
     for row in range(len(board)):
         # print(f"row: {row}")
         for col in range(len(board[0])):
             # print(f"    col: {col}")
-            if board[row][col] == "p":
+            if board[row][col] == piece:
                 # print(board[row][col])
                 found = True    
                 break
         if found:
             print(f"Row and col of p: {row}, {col}")
-            show_board(board)
             return row, col
     
 def show_board(board):
@@ -48,7 +47,9 @@ def checkBounds(rowColNum):
             print(rowColNum)
     return rowColNum
 
-def move_piece(board, old_row, old_col):
+def move_piece(board, piece):
+    old_row, old_col = findRowCol(piece)
+    
     new_row = old_row
     new_col = old_col
 
@@ -84,8 +85,8 @@ def move_piece(board, old_row, old_col):
 
 while True:
     # For example, move 'b' from (1, 2) to (0, 0)
-    old_row, old_col = findRowCol()
-
-    move_piece(board, old_row, old_col)
+    show_board(board)
+    piece = input("Which piece do you want to move?")
+    move_piece(board, piece)
 
 
