0c462c5666bc394d2215c28b6c1e47eae56bd9d0
vhasanli <vhasanov.software@gmail.com>
2025-04-09 15:27:22 -0500

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,3 +1,5 @@
+from chess_data_structures import MoveType
+
 X = 0
 Y = 1
 
@@ -14,7 +16,7 @@ def piece_rule_checker(board, player, piece, cur_pos, next_pos):
         print("It is a white_bishop")
     elif piece == 4:
         print("It is a white_rook")
-        return rook_rule_checker(player, cur_pos, next_pos, piece_at_next_location)
+        return rook_rule_checker(board, player, cur_pos, next_pos, piece_at_next_location)
     elif piece == 5:
         print("It is a white_queen")
     elif piece == 6:
@@ -33,12 +35,56 @@ def piece_rule_checker(board, player, piece, cur_pos, next_pos):
     elif piece == 12:
         print("It is a black_king")
 
-def rook_rule_checker(player, cur_pos, next_pos, piece_at_next_location):
+def move_dir_finder(cur_pos, next_pos):
+    if (cur_pos[X] > next_pos[X]) and (cur_pos[Y] == next_pos[Y]):
+        move_type = MoveType.up.value
+        print("UP move")
+    elif (cur_pos[X] < next_pos[X]) and (cur_pos[Y] == next_pos[Y]):
+        move_type = MoveType.down.value
+        print("DOWN move")
+    elif (cur_pos[Y] < next_pos[Y]) and (cur_pos[X] == next_pos[X]):
+        move_type = MoveType.right.value
+        print("RIGHT move")
+    elif (cur_pos[Y] > next_pos[Y]) and (cur_pos[X] == next_pos[X]):
+        move_type = MoveType.left.value
+        print("LEFT move")
+    elif (cur_pos[X] > next_pos[X]) and (cur_pos[Y] > next_pos[Y]):
+        print("UP and LEFT diagonal move")
+    elif (cur_pos[X] < next_pos[X]) and (cur_pos[Y] < next_pos[Y]):
+        print("DOWN and RIGHT diagonal move")
+    elif (cur_pos[X] < next_pos[X]) and (cur_pos[Y] > next_pos[Y]):
+        print("DOWN and LEFT diagonal move")
+    elif (cur_pos[X] > next_pos[X]) and (cur_pos[Y] < next_pos[Y]):
+        move_type = MoveType.up_right.value
+        print("UP and RIGHT diagonal move")
+
+    return move_type
+
+def move_search_algo(board, cur_pos, next_pos):
+    
+
+    # for row in range(cur_pos[X] - next_pos[X]):
+    #     tmp_piece = board[(cur_pos[X] - (row + 1))][next_pos[Y]]
+    #     if tmp_piece > 0 and 
+    #     print(tmp_piece)
+        
+    return True
+
+def rook_rule_checker(board, player, cur_pos, next_pos, piece_at_next_location):
     if player == 0:
+        # Forward move
+        rook_move_dir = move_dir_finder(cur_pos, next_pos)
+        # if rook move is legal then proceed, else return False
+        if (rook_move_dir >= 0) and (rook_move_dir <=4):
+            legal = move_search_algo(board, cur_pos, next_pos)
+            legal = True
+            if legal:
+                if ((piece_at_next_location == 0) or ((piece_at_next_location >= 7) and (piece_at_next_location <= 12))):
+                    print("Rook move or capture legal")
+            return True
+    else:   
         pass
-    else:
-        pass
-    pass
+    return False
 
 def pawn_rule_checker(player, cur_pos, next_pos, piece_at_next_location):
 
