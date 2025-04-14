d9508aab06efc05001971c98c3b6c5ae169019cc
vhasanli <vhasanov.software@gmail.com>
2025-04-14 15:05:59 -0500

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,12 +1,11 @@
 from chess_data_structures import Player, MoveType, PieceType, ROW, COL
 
 def is_opposite_piece(player, piece_at_next_location):
-    if (player == Player.WHITE) and (piece_at_next_location >= 7 and piece_at_next_location <= 12):
+    if (player == "W") and (piece_at_next_location >= 7 and piece_at_next_location <= 12):
         return True
-    elif (player == Player.BLACK) and (piece_at_next_location >= 1 and piece_at_next_location <= 6):
+    elif (player == "B") and (piece_at_next_location >= 1 and piece_at_next_location <= 6):
         return True
     else:
-        print("No such player or piece")
         return False
 
 def move_dir_finder(cur_pos, next_pos):
